4f3f0dd5f1b1a6a587258c96570513b232d9b123
alexfynnoneill <alex.fynn.oneill@gmail.com>
2017-01-15 17:25:12 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function askQuestions () /* roundbrackets means 'run function'! */ {
+
+
+	var firstName = prompt('What is your first name?'),
+		lastName = prompt('What is your last name?'),
+		fullName = firstName + ' ' + lastName;
+
+
+	console.log(fullName);
+
+	var age = prompt('How old are you?');
+	age = parseInt(age);
+
+	if (age>= 18) {
+
+		console.log ('User is an adult'); 
+	} 
+
+	else if (age>= 13) { 
+
+		console.log ('User is a teenager');
+	}
+
+	else { 
+
+		console.log ('User is a child');
+	}
+
+	if (firstName === 'general') {
+
+		if (lastName !== 'Assembly') {
+
+			console.log ('Hey!')
+
+		}
+
+}
+
+}
+	
+
+/* JS when page has loaded */
+$(function () {
+
+	/* when user clicks h3 element */
+	$('h3').on ('click', function () {
+
+	/* hide next element */
+	$(this).next().slideToggle(750); 
+
+	});
+
+});
